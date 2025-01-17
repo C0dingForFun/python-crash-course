1672c1e14af54b99e720c51503b4542aa5c7db53
C0dingForFun <gwibantulini8@gmail.com>
2025-01-17 12:01:08 +0200

    control flow

---
+++ flow.py
@@ -0,0 +1,34 @@
+# if statements
+# if 3 > 4:
+#     print('correct result') # false
+# elif 0 > 1:
+#     print('some other result') # false
+# elif 0 == 0 and 5 > 1:
+#     print('some other result') # true so one will display
+#     if len([1, 2, 3 ]) > 2:
+#         print('list is long enough')
+# else:
+#     print('incorrect result')
+
+# while loop
+# counter = 0
+
+# while counter <= 10:
+#     print(counter)
+#     counter += 1
+#     if counter == 5:
+#         print('counter is 5')
+# print('while loop is finished')
+
+# for loop
+# test_list = {1, 2, 3, 3}
+# test_list = {1:2, 3:4, 5:6}
+# for x in test_list.keys(): loops through the dictionary's keys
+# for x in test_list.values(): # loops through the dictionary's values
+# for x, y in test_list.items(): # loops through the dictionary's items
+#     print(x)
+#     print(y)
+
+# truthy and falsy
+if 0:
+    print('something')
